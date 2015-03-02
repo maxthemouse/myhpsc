c00613dd8091378c299b2c79c51b280435d07e10
Adam Webb <adam.webb@lightsource.ca>
2015-03-02 16:18:27 -0600

    added debug flag, return value

--- lectures/lecture4/mysqrt.py
+++ lectures/lecture4/mysqrt.py
@@ -2,7 +2,7 @@
 Module for approximating sqrt.
 """
 
-def sqrt2(x):
+def sqrt2(x, debug=False):
     """
     Newton's method
     """
@@ -10,10 +10,13 @@ def sqrt2(x):
     kmax = 100
     tol = 1.e-14
     for k in range(kmax):
-        print "Before iteration %s, s = %20.15f." % (k, s)
+        if debug:
+            print "Before iteration %s, s = %20.15f." % (k, s)
         s0 = s
         s = 0.5 * (s + x /s)
         delta_s = s - s0
         if abs(delta_s / x) < tol: 
             break
-    print "After %s iterations, s = %20.15f." % (k+1, s)
+    if debug:
+        print "After %s iterations, s = %20.15f." % (k+1, s)
+    return s
