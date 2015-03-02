7c4e3adbaf2683ba0e3c6ca1e57bd9e65664ccf7
Adam Webb <adam.webb@lightsource.ca>
2015-03-02 16:09:33 -0600

    copied mysqrt.py from lecture3 to lecture4

---
+++ lectures/lecture4/mysqrt.py
@@ -0,0 +1,19 @@
+"""
+Module for approximating sqrt.
+"""
+
+def sqrt2(x):
+    """
+    Newton's method
+    """
+    s = 1.
+    kmax = 100
+    tol = 1.e-14
+    for k in range(kmax):
+        print "Before iteration %s, s = %20.15f." % (k, s)
+        s0 = s
+        s = 0.5 * (s + x /s)
+        delta_s = s - s0
+        if abs(delta_s / x) < tol: 
+            break
+    print "After %s iterations, s = %20.15f." % (k+1, s)
