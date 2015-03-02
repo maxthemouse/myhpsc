4d9b6bcd7b1933ac314705472546ad7fdf8d0d1b
Adam Webb <adam.webb@lightsource.ca>
2015-03-02 16:37:22 -0600

    added tests of input

--- lectures/lecture4/mysqrt.py
+++ lectures/lecture4/mysqrt.py
@@ -6,6 +6,13 @@ def sqrt2(x, debug=False):
     """
     Newton's method
     """
+    from numpy import nan
+    if x == 0:
+        return 0.
+    elif x < 0:
+        print "*** Error, x must be nonnegative"
+        return nan
+    assert x > 0. and type(x) is float or type(x) is int, "Unrecognized input"
     s = 1.
     kmax = 100
     tol = 1.e-14
