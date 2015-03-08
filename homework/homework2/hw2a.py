6fcc17c96bc7d66f20b066d9498b5af4956e14a7
Adam Webb <adam.webb@lightsource.ca>
2015-03-08 09:37:19 -0700

    make format changes to conform to pep8

--- homework/homework2/hw2a.py
+++ homework/homework2/hw2a.py
@@ -20,22 +20,22 @@ A = np.array([[1., -1., 1.], [1., 1., 1.], [1., 2., 4.]])
 b = yi
 
 # Solve the system:
-c = solve(A,b)
+c = solve(A, b)
 
 print "The polynomial coefficients are:"
 print c
 
 # Plot the resulting polynomial:
-x = np.linspace(-2,3,1001)   # points to evaluate polynomial
-y = c[0] + c[1]*x + c[2]*x**2
+x = np.linspace(-2, 3, 1001)   # points to evaluate polynomial
+y = c[0] + c[1] * x + c[2] * x ** 2
 
 plt.figure(1)       # open plot figure window
 plt.clf()           # clear figure
-plt.plot(x,y,'b-')  # connect points with a blue line
+plt.plot(x, y, 'b-')  # connect points with a blue line
 
 # Add data points  (polynomial should go through these points!)
-plt.plot(xi,yi,'ro')   # plot as red circles
-plt.ylim(-2,8)         # set limits in y for plot
+plt.plot(xi, yi, 'ro')   # plot as red circles
+plt.ylim(-2, 8)         # set limits in y for plot
 
 plt.title("Data points and interpolating polynomial")
 
--- homework/homework2/hw2b.py
+++ homework/homework2/hw2b.py
@@ -10,7 +10,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.linalg import solve
 
-def quad_interp(xi,yi):
+
+def quad_interp(xi, yi):
     """
     Quadratic interpolation.  Compute the coefficients of the polynomial
     interpolating the points (xi[i],yi[i]) for i = 0,1,2.
@@ -25,13 +26,13 @@ def quad_interp(xi,yi):
     assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
 
     error_message = "xi and yi should have length 3"
-    assert len(xi)==3 and len(yi)==3, error_message
+    assert len(xi) == 3 and len(yi) == 3, error_message
 
     # Set up linear system to interpolate through data points:
-    A = np.vstack([np.ones(3), xi, xi**2]).T
+    A = np.vstack([np.ones(3), xi, xi ** 2]).T
 
-    ### Fill in this part to compute c ###
-    c = solve(A,yi)
+    # compute c #
+    c = solve(A, yi)
 
     return c
 
@@ -42,22 +43,23 @@ def plot_quad(xi, yi):
     """
     c = quad_interp(xi, yi)
     x = np.linspace(xi.min() - 1, xi.max() + 1, 1000)
-    y = c[0] + c[1]*x + c[2]*x**2
+    y = c[0] + c[1] * x + c[2] * x ** 2
 
     plt.figure(1)       # open plot figure window
     plt.clf()           # clear figure
-    plt.plot(x,y,'b-')  # connect points with a blue line
+    plt.plot(x, y, 'b-')  # connect points with a blue line
 
     # Add data points  (polynomial should go through these points!)
-    plt.plot(xi,yi,'ro')   # plot as red circles
+    plt.plot(xi, yi, 'ro')   # plot as red circles
     plt.ylim(y.min() - 1, y.max() + 1)         # set limits in y for plot
 
     plt.title("Data points and interpolating polynomial")
 
     plt.savefig('quadratic.png')   # save figure as .png file
-    
+
     # return c so that this function has same input/output as quad_interp
     return c
+
 
 def cubic_interp(xi, yi):
     """
@@ -73,15 +75,16 @@ def cubic_interp(xi, yi):
     assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
 
     error_message = "xi and yi should have length 3"
-    assert len(xi)==4 and len(yi)==4, error_message
+    assert len(xi) == 4 and len(yi) == 4, error_message
 
     # Set up linear system to interpolate through data points:
-    A = np.vstack([np.ones(4), xi, xi**2, xi**3]).T
+    A = np.vstack([np.ones(4), xi, xi ** 2, xi ** 3]).T
 
-    ### Fill in this part to compute c ###
-    c = solve(A,yi)
+    # compute c #
+    c = solve(A, yi)
 
     return c
+
 
 def plot_cubic(xi, yi):
     """
@@ -89,20 +92,20 @@ def plot_cubic(xi, yi):
     """
     c = cubic_interp(xi, yi)
     x = np.linspace(xi.min() - 1, xi.max() + 1, 1000)
-    y = c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3
+    y = c[0] + c[1] * x + c[2] * x ** 2 + c[3] * x ** 3
 
     plt.figure(1)       # open plot figure window
     plt.clf()           # clear figure
-    plt.plot(x,y,'b-')  # connect points with a blue line
+    plt.plot(x, y, 'b-')  # connect points with a blue line
 
     # Add data points  (polynomial should go through these points!)
-    plt.plot(xi,yi,'ro')   # plot as red circles
+    plt.plot(xi, yi, 'ro')   # plot as red circles
     plt.ylim(y.min() - 1, y.max() + 1)         # set limits in y for plot
 
     plt.title("Data points and interpolating polynomial")
 
     plt.savefig('cubic.png')   # save figure as .png file
-    
+
     # return c so that this function has same input/output as quad_interp
     return c
 
@@ -121,13 +124,13 @@ def poly_interp(xi, yi):
     assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
 
     error_message = "xi and yi should have equal length"
-    assert len(xi)==len(yi), error_message
+    assert len(xi) == len(yi), error_message
 
     # Set up linear system to interpolate through data points:
-    A = np.vstack([xi**j for j in range(len(xi))]).T
+    A = np.vstack([xi ** j for j in range(len(xi))]).T
 
-    ### Fill in this part to compute c ###
-    c = solve(A,yi)
+    # compute c #
+    c = solve(A, yi)
 
     return c
 
@@ -139,53 +142,54 @@ def plot_poly(xi, yi):
     c = poly_interp(xi, yi)
     x = np.linspace(xi.min() - 1, xi.max() + 1, 1000)
     n = len(xi)
-    y = c[n-1]
-    for j in range(n-1, 0, -1):
-        y = y*x + c[j-1]
+    y = c[n - 1]
+    for j in range(n - 1, 0, -1):
+        y = y * x + c[j - 1]
 
     plt.figure(1)       # open plot figure window
     plt.clf()           # clear figure
-    plt.plot(x,y,'b-')  # connect points with a blue line
+    plt.plot(x, y, 'b-')  # connect points with a blue line
 
     # Add data points  (polynomial should go through these points!)
-    plt.plot(xi,yi,'ro')   # plot as red circles
+    plt.plot(xi, yi, 'ro')   # plot as red circles
     plt.ylim(y.min() - 1, y.max() + 1)         # set limits in y for plot
 
     plt.title("Data points and interpolating polynomial")
 
     plt.savefig('poly.png')   # save figure as .png file
-    
+
     # return c so that this function has same input/output as quad_interp
     return c
 
-    
+
 def test_quad1():
     """
     Test code, no return value or exception if test runs properly.
     """
     xi = np.array([-1.,  0.,  2.])
-    yi = np.array([ 1., -1.,  7.])
-    c = plot_quad(xi,yi)
+    yi = np.array([1., -1.,  7.])
+    c = plot_quad(xi, yi)
     c_true = np.array([-1.,  0.,  2.])
     print "c =      ", c
     print "c_true = ", c_true
     # test that all elements have small error:
     assert np.allclose(c, c_true), \
-        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+        "Incorrect result, c = %s, Expected: c = %s" % (c, c_true)
+
 
 def test_quad2():
     """
     Test code, no return value or exception if test runs properly.
     """
     xi = np.array([-1.,  1.,  2.])
-    yi = np.array([ 0., 4.,  3.])
-    c = plot_quad(xi,yi)
+    yi = np.array([0., 4.,  3.])
+    c = plot_quad(xi, yi)
     c_true = np.array([3.,  2., -1.])
     print "c =      ", c
     print "c_true = ", c_true
     # test that all elements have small error:
     assert np.allclose(c, c_true), \
-        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+        "Incorrect result, c = %s, Expected: c = %s" % (c, c_true)
 
 
 def test_cubic1():
@@ -194,13 +198,13 @@ def test_cubic1():
     """
     xi = np.array([-2., -1., 0., 2.])
     yi = np.array([-1., 0., -1., 15.])
-    c = plot_cubic(xi,yi)
+    c = plot_cubic(xi, yi)
     c_true = np.array([-1., 0., 2., 1.])
     print "c =      ", c
     print "c_true = ", c_true
     # test that all elements have small error:
     assert np.allclose(c, c_true), \
-        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+        "Incorrect result, c = %s, Expected: c = %s" % (c, c_true)
 
 
 def test_cubic2():
@@ -209,13 +213,13 @@ def test_cubic2():
     """
     xi = np.array([-2, -1.,  1.,  2.])
     yi = np.array([11., 2., 2., -13.])
-    c = plot_cubic(xi,yi)
+    c = plot_cubic(xi, yi)
     c_true = np.array([3.,  2., -1., -2])
     print "c =      ", c
     print "c_true = ", c_true
     # test that all elements have small error:
     assert np.allclose(c, c_true), \
-        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+        "Incorrect result, c = %s, Expected: c = %s" % (c, c_true)
 
 
 def test_poly1():
@@ -223,75 +227,77 @@ def test_poly1():
     Test code, no return value or exception if test runs properly.
     """
     xi = np.array([-1.,  0.,  2.])
-    yi = np.array([ 1., -1.,  7.])
-    c = poly_interp(xi,yi)
+    yi = np.array([1., -1.,  7.])
+    c = poly_interp(xi, yi)
     c_true = np.array([-1.,  0.,  2.])
     print "c =      ", c
     print "c_true = ", c_true
     # test that all elements have small error:
     assert np.allclose(c, c_true), \
-        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+        "Incorrect result, c = %s, Expected: c = %s" % (c, c_true)
     xi = np.array([-1.,  1.,  2.])
-    yi = np.array([ 0., 4.,  3.])
-    c = poly_interp(xi,yi)
+    yi = np.array([0., 4.,  3.])
+    c = poly_interp(xi, yi)
     c_true = np.array([3.,  2., -1.])
     print "c =      ", c
     print "c_true = ", c_true
     # test that all elements have small error:
     assert np.allclose(c, c_true), \
-        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+        "Incorrect result, c = %s, Expected: c = %s" % (c, c_true)
     xi = np.array([-2., -1., 0., 2.])
     yi = np.array([-1., 0., -1., 15.])
-    c = poly_interp(xi,yi)
+    c = poly_interp(xi, yi)
     c_true = np.array([-1., 0., 2., 1.])
     print "c =      ", c
     print "c_true = ", c_true
     # test that all elements have small error:
     assert np.allclose(c, c_true), \
-        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+        "Incorrect result, c = %s, Expected: c = %s" % (c, c_true)
     xi = np.array([-2, -1.,  1.,  2.])
     yi = np.array([11., 2., 2., -13.])
-    c = poly_interp(xi,yi)
+    c = poly_interp(xi, yi)
     c_true = np.array([3.,  2., -1., -2])
     print "c =      ", c
     print "c_true = ", c_true
     # test that all elements have small error:
     assert np.allclose(c, c_true), \
-        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+        "Incorrect result, c = %s, Expected: c = %s" % (c, c_true)
     xi = np.array([-3., -2, -1.,  1.,  3.])
     yi = np.array([123., 27., 3., 3., 27.])
-    c = poly_interp(xi,yi)
+    c = poly_interp(xi, yi)
     c_true = np.array([3.,  2., -1., -2, 1])
     print "c =      ", c
     print "c_true = ", c_true
     # test that all elements have small error:
     assert np.allclose(c, c_true), \
-        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+        "Incorrect result, c = %s, Expected: c = %s" % (c, c_true)
     xi = np.array([-3, -2, -1.,  1.,  2., 3.])
     yi = np.array([-201., -21., 1., 3., 19., 189.])
-    c = poly_interp(xi,yi)
+    c = poly_interp(xi, yi)
     c_true = np.array([3.,  2., -1., -2, 0, 1])
     print "c =      ", c
     print "c_true = ", c_true
     # test that all elements have small error:
     assert np.allclose(c, c_true), \
-        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+        "Incorrect result, c = %s, Expected: c = %s" % (c, c_true)
+
 
 def test_poly2():
     xi = np.array([-3, -2, -1.,  1.,  2., 3.])
     yi = np.array([-201., -21., 1., 3., 19., 189.])
-    c = plot_poly(xi,yi)
+    c = plot_poly(xi, yi)
     c_true = np.array([3.,  2., -1., -2, 0, 1])
     print "c =      ", c
     print "c_true = ", c_true
     # test that all elements have small error:
     assert np.allclose(c, c_true), \
-        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+        "Incorrect result, c = %s, Expected: c = %s" % (c, c_true)
 
-        
-if __name__=="__main__":
+
+if __name__ == "__main__":
     # "main program"
-    # the code below is executed only if the module is executed at the command line,
+    # the code below is executed only if the module is executed
+    # at the command line,
     #    $ python demo2.py
     # or run from within Python, e.g. in IPython with
     #    In[ ]:  run demo2
