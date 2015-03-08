48923086d84a54330749e55ca98d3310d2580dd9
Adam Webb <adam.webb@lightsource.ca>
2015-03-08 08:37:21 -0700

    homework 2, add cubic

--- homework/homework2/hw2b.py
+++ homework/homework2/hw2b.py
@@ -60,11 +60,51 @@ def plot_quad(xi, yi):
     return c
 
 def cubic_interp(xi, yi):
-    pass
+    """
+    Cubic interpolation.  Compute the coefficients of the polynomial
+    interpolating the points (xi[i],yi[i]) for i = 0,1,2,3.
+    Returns c, an array containing the coefficients of
+      p(x) = c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3.
+    """
 
+    # check inputs and print error message if not valid:
 
-def plot_qubic(xi, yi):
-    pass
+    error_message = "xi and yi should have type numpy.ndarray"
+    assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
+
+    error_message = "xi and yi should have length 3"
+    assert len(xi)==4 and len(yi)==4, error_message
+
+    # Set up linear system to interpolate through data points:
+    A = np.vstack([np.ones(4), xi, xi**2, xi**3]).T
+
+    ### Fill in this part to compute c ###
+    c = solve(A,yi)
+
+    return c
+
+def plot_cubic(xi, yi):
+    """
+    Plot quadratic interpolation. Compute the polynomial and make a plot.
+    """
+    c = cubic_interp(xi, yi)
+    x = np.linspace(xi.min() - 1, xi.max() + 1, 1000)
+    y = c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3
+
+    plt.figure(1)       # open plot figure window
+    plt.clf()           # clear figure
+    plt.plot(x,y,'b-')  # connect points with a blue line
+
+    # Add data points  (polynomial should go through these points!)
+    plt.plot(xi,yi,'ro')   # plot as red circles
+    plt.ylim(y.min() - 1, y.max() + 1)         # set limits in y for plot
+
+    plt.title("Data points and interpolating polynomial")
+
+    plt.savefig('cubic.png')   # save figure as .png file
+    
+    # return c so that this function has same input/output as quad_interp
+    return c
 
 
 def poly_interp(xi, yi):
@@ -105,7 +145,18 @@ def test_quad2():
 
 
 def test_cubic1():
-    pass
+    """
+    Test code, no return value or exception if test runs properly.
+    """
+    xi = np.array([-2., -1., 0., 2.])
+    yi = np.array([-1., 0., -1., 15.])
+    c = plot_cubic(xi,yi)
+    c_true = np.array([-1., 0., 2., 1.])
+    print "c =      ", c
+    print "c_true = ", c_true
+    # test that all elements have small error:
+    assert np.allclose(c, c_true), \
+        "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
 
 
 def test_cubic2():
@@ -128,4 +179,4 @@ if __name__=="__main__":
     #    In[ ]:  run demo2
     # not if the module is imported.
     print "Running test..."
-    test_quad1()
+    test_cubic1()
