f8852e7f84d28b4afd7049c4cba344c442ee45b6
Adam Webb <adam.webb@lightsource.ca>
2015-05-28 19:48:28 -0600

    part 2 of project works

--- homework/project/part2/quadrature_mc.f90
+++ homework/project/part2/quadrature_mc.f90
@@ -3,33 +3,37 @@ module quadrature_mc
 
 contains
 
-    function quad_mc(g, a, b, ndim, npoints)
+function quad_mc(g, a, b, ndim, npoints)
 
     implicit none
+    real(kind=8),external :: g
     integer, intent(in) :: ndim, npoints
     real(kind=8), intent(in) :: a(ndim), b(ndim)
-    real(kind=8), external :: g
-    real(kind=8) :: rand_x(ndim*npoints), rand_y(ndim*npoints)
-    real(kind=8) :: product, sum
-    integer :: i, N
+    real(kind=8) :: quad_mc
 
+    integer :: i
 
- 
-    rand_y = g(rand_x, ndim*npoints)
+    real(kind=4), allocatable :: r(:), rr(:)
+    real(kind=8) :: gsum, volume
+    real(kind=8), allocatable :: x(:)
 
-    product = 1.0d0
-    N = ndim*npoints
-    do i = 1, ndim
-	product = product*(b(i) - a(i))
-    enddo
+    allocate(r(ndim*npoints))
+    allocate(x(ndim), rr(ndim))
 
-    sum = 0.0d0
-    do i = 1, N
-	sum = sum + rand_y(i)
-    enddo
+    gsum = 0.d0
 
-    quad_mc = product*sum/N
+    volume = product(b-a)
 
-    end function quad_mc
+    call random_number(r)
+    do i=1,npoints
+        rr = r((i-1)*ndim + 1 : i*ndim)  ! next ndim elements of r
+        x = a + rr*(b-a)   ! sets ndim elements of x elementwise
+        gsum = gsum + g(x,ndim)
+        enddo
+    quad_mc = volume * gsum /  npoints
+
+    deallocate(x,r,rr)
+
+end function quad_mc
 
 end module quadrature_mc
